2ca6bfc76a0e685e1bb8d0ba6aa8be54cb120b39
Rahul Khetal <rahul.khetal@mastek.com>
2020-06-19 00:37:44 +0100

    Initial Commit

--- automationrunner/adhocrunner.bat
+++
@@ -1,8 +0,0 @@
-set projectLocation=D:\Docs\Rahul\VitaminShopee\Automation_Project
-cd %projectLocation%
-call mvn -f adhoc_pom.xml clean -Dmaven.clean.failOnError=false install
-call mvn exec:java@RunChangeExecutionStatus
-call mvn exec:java@RunDumpReportToDB
-call mvn exec:java@RunExcelReportGenerator
-echo "Generating Report..."
-timeout /t 5
