2255787dbafdeb48d8d226afc6170780f0cbcd33
Rupeshk14312 <Rupeshk14312@IN-MH1DSW102091.mastek.com>
2020-12-15 11:55:19 +0530

    Added Password field (Authentication integration)

--- automation-ui/src/main/java/com/handlers/UserHandler.java
+++ automation-ui/src/main/java/com/handlers/UserHandler.java
@@ -3,9 +3,14 @@ package com.handlers;
 import hibernate.HibernateUtil;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+
+import org.apache.commons.codec.digest.Md5Crypt;
 
 import beans.Users;
 
@@ -14,13 +19,18 @@ import com.sun.net.httpserver.HttpExchange;
 
 public class UserHandler
 {
-	public static String validateUser(String emailId, HttpExchange httpExchange) throws IOException
+	public static String validateUser(String emailId, String password, HttpExchange httpExchange) throws IOException
 	{
 		StringBuilder contentBuilder = new StringBuilder();
 		String content = "";
-		List<Users> user=UserDao.getUserWithEmailAddress(emailId);
-		
-		if (!user.isEmpty())
+		List<Users> user=UserDao.getUserWithEmailAddress(emailId,password);
+			
+			String pwd=null;
+			for(Users user1:user){
+				pwd = user1.getPass();				
+			}
+			
+		if (!user.isEmpty()&& pwd.equals(genaratePassword(password)))
 		{
 			BufferedReader in = new BufferedReader(new FileReader("src/main/resources/html/main.html"));
 			String str;
@@ -29,11 +39,13 @@ public class UserHandler
 				contentBuilder.append(str);
 			}
 			String role=null;
-			for(Users user1:user){
+			for(Users user1:user)
+			{
 				role= user1.getRole();
 			}
-			
+
 			content = contentBuilder.toString().replaceAll("LOGIN_USER_ROLE", "<div hidden id='loginUserRole'>"+role+"</div>");
+
 		}
 		else
 		{
@@ -43,11 +55,51 @@ public class UserHandler
 			{
 				contentBuilder.append(str);
 			}
-			content = contentBuilder.toString().replaceAll("ERROR_MESSAGE", "<div class='alert alert-danger'><span>Please enter a valid Email address.</span></div>");
+			content = contentBuilder.toString().replaceAll("ERROR_MESSAGE", "<div class='alert alert-danger'><span>Please enter a valid credentials.</span></div>");
+ 
+			//content = showError("Please enter a valid Email address.");
 		}
 		System.out.println("content of main.html " + content);
 
 		return content;
 	}
 
+	private static String showError(String errorMessage) throws IOException {
+		StringBuilder contentBuilder = new StringBuilder();
+		BufferedReader in = new BufferedReader(new FileReader("src/main/resources/html/login.html"));
+		String str;
+		while ((str = in.readLine()) != null)
+		{
+			contentBuilder.append(str);
+		}
+		return contentBuilder.toString().replaceAll("ERROR_MESSAGE", "<div class='alert alert-danger'><span>" + errorMessage +"</span></div>");		
+	}
+	
+	public static String genaratePassword(String password) {
+		String generatedPassword = null;
+	try {
+		// Create MessageDigest instance for MD5
+		            MessageDigest md;					
+					md = MessageDigest.getInstance("MD5");
+					
+		            //Add password bytes to digest
+		            md.update(password.getBytes());
+		            //Get the hash's bytes 
+		            byte[] bytes = md.digest();
+		            //This bytes[] has bytes in decimal format;
+		            //Convert it to hexadecimal format
+		            StringBuilder sb = new StringBuilder();
+		            for(int i=0; i< bytes.length ;i++)
+		            {
+		                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+		            }
+		            
+		            //Get complete hashed password in hex format
+		            generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	return generatedPassword;
+	}
 }
