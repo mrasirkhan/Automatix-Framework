710a1ea3bcc77e73b5391c5c45a0176bbe50b606
unknown <rupeshkumar.borase@mastek.com>
2021-01-18 14:20:22 +0530

    Testing

--- automation-project/src/main/java/utilities/JavaScriptExecutor.java
+++ automation-project/src/main/java/utilities/JavaScriptExecutor.java
@@ -9,7 +9,6 @@ public class JavaScriptExecutor
 {
 	public static String getValueForJavaScript(WebDriver driver,String javaScript)
 	{
-		System.out.println("JavascriptExecutor");
     	JavascriptExecutor js = (JavascriptExecutor)driver;
     	return (String) js.executeScript(javaScript);
 	}
